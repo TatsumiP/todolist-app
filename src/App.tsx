7f87bf50713f58a0dffb0359e42b1f57a8f8858a
Tatsumi <tatumistudio@gmail.com>
2022-07-14 16:37:53 +0900

    [add]minimum functions implemented

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,45 @@ function App() {
   }
 
   const handleSubmit =  (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault(); // reload Avoidance
+    e.preventDefault(); // reload Avoidance. I forget often.
+
+    // creating new Todo
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: todos.length,
+      checked: false,
+    };
+
+    setInputValue("");
+    setTodos([newTodo, ...todos]); // new todo stored in all other todos
+  };
+
+  const handleEdit = (id: number, inputValue: string) => {
+    const newTodos = todos.map((todo) => {
+      if(todo.id === id) {
+        todo.inputValue = inputValue;
+      }
+      return todo; //newTodos is void.todos is array of todo.
+    });
+
+    setTodos(newTodos);
+  };
+
+  const handleChecked = (id: number, checked: boolean) => {
+    const newTodos = todos.map((todo) => {
+      if(todo.id === id) {
+        todo.checked = !checked;
+      }
+      return todo; //newTodos is void.todos is array of todo.
+    });
+
+    setTodos(newTodos);
+  }
+
+  const handleDelete = (id: number) => {
+    // pick up all ids except the one you delete
+    const newTodos = todos.filter((todo) =>  todo.id !== id);
+    setTodos(newTodos);
   }
 
   return (
@@ -32,11 +70,35 @@ function App() {
         <form onSubmit={(e) => handleSubmit(e)}>
           <input
             type="text" onChange={(e) => handleChange(e)}
-            className="inputText" />
+            className="inputText"
+          />
           <input
             type="submit" value="create"
-            className="submitButton"/>
+            className="submitButton"
+          />
         </form>
+        <ul className="todoList">
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input
+                type="text" onChange={(e) => handleEdit(todo.id, e.target.value)}
+                value={todo.inputValue} className="inputText"
+                disabled={todo.checked}
+              />
+              <input
+                type="checkbox"
+                onChange={(e) => handleChecked(todo.id, todo.checked)}
+                className="submitButton"
+              />
+              <button
+                onClick={() => handleDelete(todo.id)}
+                className="deleteButton"
+              >
+                delete
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
 
     </div>
@@ -46,8 +108,13 @@ function App() {
 export default App;
 
 // this is the process to create the App.
+
+// Design of individual data
 // We need typescript data of todoname, todoId , boolean weather completed or not.
 // We need the three States. So we create three functions to get state.
 // First function is sending inputname.
 // Second function is creating Id.
 // third function is switching check of completed.
+
+// Design of set of data
+//
